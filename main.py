56f0b12d8e701e2ef99f7f2a6b7a13af95985727
SM13121312 <strahinja09mitic@gmail.com>
2025-07-28 18:55:44 +0200

    rgfrgrg
    grgrgr

--- main.py
+++ main.py
@@ -243,6 +243,8 @@ def izvestaji():
         
 
 def mesecna_nagrada():
+    danasnji_datum = date.today()
+    za_mesec_datum = date.today() - timedelta(days=30)
     cursor.execute('''UPDATE korisnici
                     SET status_korisnika = 'aktivan', paket = 'premium', datum_aktivacije = ?, datum_isteka = ?
                     WHERE status_korisnika = 'neaktivan' AND korisnicko_ime IN (
